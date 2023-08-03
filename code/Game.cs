35f438429e7a75a604d40741d2c7d0677c453050
Nikk Jackson <NikkolasJ@Outlook.com>
2023-08-03 19:19:55 -0400

    Feat: Added job menu

--- code/Game.cs
+++ code/Game.cs
@@ -12,8 +12,8 @@ namespace MyGame;
 
 /// <summary>
 /// This is your game class. This is an entity that is created serverside when
-/// the game starts, and is replicated to the client. 
-/// 
+/// the game starts, and is replicated to the client.
+///
 /// You can use this to create things like HUDs and declare which player class
 /// to use for spawned players.
 /// </summary>
@@ -29,10 +29,12 @@ public partial class MyGame : Sandbox.GameManager
 		if ( Game.IsClient )
 		{
 			Game.RootPanel = new Hud();
+			Game.RootPanel = new JobMenu();
 		}
+
 		LoadJobs();
 	}
-	
+
 	/// <summary>
 	/// A client has joined the server. Make them a pawn to play with
 	/// </summary>
